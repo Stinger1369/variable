e2c339bcb774322a1dddde406d90459e03f22b6c
bilel zaaraoui <bilelzara@gmail.com>
2023-11-09 12:11:51 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+
+  /*
+    🐦 DAVID Bruno Twitter profile 🐦
+
+    🏁 Please take this quick challenge and rename carefully all the variable, and fix this broken code by assigning the correct 
+    variable keyword you have learned in the quest.
+    ex: const name = "David";
+
+  */
+
+  //const name = "David"; // ✅ Good!  
+  const firstname = "David"; // je prefere utiliser name pour le nom et firstname pour le prenom
+  console.log(firstname); // ✅ Good!
+  //lastname = "Bruno"; // ❌ the keyword to declare the variable is missing 
+  const lastName = "Bruno"; // ✅ Good! 
+  console.log(lastName +" "+ firstname); // ✅ Good!
+  
+  const biography = "Hi, I'm David Bruno from SF, I like to cook and meet new people."; // ❌ the variable name is not explicit. (this is David's biography)
+ console.log(biography); // ✅ Good!
+
+  const davidImageUrl = "http://www.go.com/davif.png"; // ❌ the variable name is not explicit and the keyword is missing (this is David's profile picture)
+ console.log(davidImageUrl); // ✅ Good!
+  const locatiion = "San Francisco"; // ❌ the variable name is not explicit and the keyword is missing (try to guess what "San Francisco" could refer to)
+console.log(locatiion); // ✅ Good!
+  let followers = 109; // ❌ the keyword to declare the variable is missing 
+  let following = 200; // ❌ the keyword to declare the variable is missing 
+
+// 🏁 Exercise 02 - David is following one more account increment the total of following account
+following = following + 1; // ✅ Good!
+following += 1; // ✅ Good!
+following++; // ✅ Good!
+console.log(following); // ✅ Good!
